027346d8ef33c72d79e7bc1b9cd04625a4eca376
EdYazbec <152457091+EdYazbec@users.noreply.github.com>
2025-02-06 21:29:08 -0500

    forgot to set the setpoint in the command

--- src/main/java/frc/robot/commands/SetElevatorPosition.java
+++ src/main/java/frc/robot/commands/SetElevatorPosition.java
@@ -9,34 +9,36 @@ import frc.robot.subsystems.Elevator;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class SetElevatorPosition extends Command {
-  /* Declare all the things this command needs */
-  private Elevator elevatorSubsystem;
-  private double setpoint;
+    /* Declare all the things this command needs */
+    private Elevator elevatorSubsystem;
+    private double setpoint;
 
-  /** Creates a new SetElevatorPosition. */
-  public SetElevatorPosition(Elevator elevatorSub, double commandedSetPoint) {
-    elevatorSubsystem = elevatorSub;
-    setpoint = commandedSetPoint;
-    
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(elevatorSubsystem);
-  }
+    /** Creates a new SetElevatorPosition. */
+    public SetElevatorPosition(Elevator elevatorSub, double commandedSetPoint) {
+      elevatorSubsystem = elevatorSub;
+      setpoint = commandedSetPoint;
+      
+      // Use addRequirements() here to declare subsystem dependencies.
+      addRequirements(elevatorSubsystem);
+    }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+      elevatorSubsystem.setPosition(setpoint);
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return elevatorSubsystem.atPosition();
-  }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return elevatorSubsystem.atPosition();
+    }
 }
