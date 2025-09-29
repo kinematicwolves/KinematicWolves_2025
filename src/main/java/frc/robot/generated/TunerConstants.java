33223ef27b77a1d70c2b05f9d14cf4c7553bbb6b
kinematicwolves <kinematicwolves5577@gmail.com>
2025-09-29 18:18:48 -0400

    9/29 blue side auto PERFECT

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -74,7 +74,9 @@ public class TunerConstants {
 
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
-    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(2.8/2);
+    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(2.8
+    
+    );
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
